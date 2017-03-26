a3fab3c2129faf381554ab2c7458072256601307
Sidharth Bhasin <bhasinsidharth97@gmail.com>
2017-03-25 23:54:29 -0700

    Create largestDividable.js

---
+++ src/largestDividable.js
@@ -0,0 +1,13 @@
+var largestDividable = (area) => {
+  for (let i = Math.sqrt(area) | 0; i > 0; i--) {
+    if (area % i === 0) return i;
+  }
+}
+
+var constructRectangle = function(area) {
+  if (area === 0) return [0, 0];
+
+  let w = largestDividable(area);
+  let l = area / w;
+  return [l, w];
+};
