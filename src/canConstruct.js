5667567e51df81bd723ca7806ea71503f4f18c0d
bhasin11 <bhasinsidharth97@gmail.com>
2017-03-27 01:45:27 -0700

    New Files

--- src/canConstruct.js
+++ src/canConstruct.js
@@ -1,19 +1,36 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
 var canConstruct = function(ransomNote, magazine) {
-    var ransomLen = ransomNote.length;
-    var magazineLen = magazine.length;
-    var copyRansomNote = ransomNote;
-    for(var i = 0; i<ransomLen ; i++) {
-        var c = ransomNote.charAt(i);
-        if(magazine.indexOf(c) > -1) {
-            magazine = magazine.replace(c, '');  
-            copyRansomNote = copyRansomNote.replace(c, '');  
-        } else {
-            return false;
-        }
-    }
     
-    if (copyRansomNote === '' && ((magazineLen - ransomLen) === magazine.length)) { 
-        return true;
+    /*
+     * The approach is to remove common characters of 
+     * both the strings. If we find a character which 
+     * is not common, we return false. After the 
+     * Ransom Note string iteration is over, we also 
+     * check if the difference between lengths of 
+     * initial strings is equal to characters left 
+     * in the magazine string. If yes, we return true.
+     * That's because there could be some extra 
+     * characters in the magazine string when compared
+     * to ransom note.
+     */
+    
+    var ransomLength = ransomNote.length;
+    var magLength = magazine.length;
+    var copy = ransomNote;
+    
+    for(var i=0;i<ransomLength;i++){
+        var char = ransomNote.charAt(i);
+        if(magazine.indexOf(char) != -1){
+            magazine = magazine.replace(char, '');
+            copy = copy.replace(char, '');
+        }
+        else return false;
     }
+    if(copy.length === 0 && 
+        magazine.length === (magLength - ransomLength)) return true;
     return false;
 };
--- src/minMoves.js
+++ src/minMoves.js
@@ -1,3 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var minMoves = function(nums) {
-  return nums.reduce((s, n) => s + n, -nums.length * Math.min(...nums))
-}
+    
+    /*
+     * The approach is to find the minimum value of the array.
+     * Then subtract it from all other values. Add each difference 
+     * to a variable. Finally, the value of this variable will be
+     * minimum moves required.
+     */
+    
+    var count = 0, min = Number.MAX_VALUE;
+    
+    for(var i=0;i<nums.length;i++){
+        min = nums[i] < min ? nums[i] : min;
+    }
+    
+    for(i=0;i<nums.length;i++){
+        count += (nums[i] - min);
+    }
+    return count;
+};
